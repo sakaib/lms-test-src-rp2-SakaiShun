ad49b37f3ce7c66c1e64fee6fc7f9f888f534fa4
sakai <brain.sakai.s@gmail.com>
2026-03-23 09:10:30 +0900

    add:Case01.java ログイン画面への遷移テストコード追加
    add:Case02.java ログイン画面への遷移テストコード追加
    DBに登録されていないユーザーでログインするテストコード追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,23 +1,53 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import jp.co.sss.lms.controller.LoginController;
+import jp.co.sss.lms.dto.InfoDto;
+import jp.co.sss.lms.service.InfoService;
+import jp.co.sss.lms.util.LoginUserUtil;
 
 /**
  * 結合テスト ログイン機能①
  * ケース01
  * @author holy
  */
+@SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
 public class Case01 {
+
+	public MockMvc mockMvc;
+
+	public MockHttpServletRequestBuilder getRequest;
+
+	@Mock
+	private LoginUserUtil loginUserUtil;
+
+	@Mock
+	private InfoService infoService;
 
 	/** 前処理 */
 	@BeforeAll
@@ -31,11 +61,43 @@ public class Case01 {
 		closeDriver();
 	}
 
+	@InjectMocks
+	private LoginController loginController;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(loginController).build();
+	}
+
+	@AfterEach
+	public void tearDown() {
+
+	}
+
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		MockHttpSession session = new MockHttpSession();
+		ReflectionTestUtils.setField(loginController, "session", session);
+
+		when(loginUserUtil.isLogin()).thenReturn(false);
+		when(infoService.getInfo()).thenReturn(new InfoDto());
+
+		getRequest = MockMvcRequestBuilders.get("/");
+
+		try {
+			mockMvc.perform(getRequest)
+					.andExpect(status().isOk())
+					.andExpect(view().name("login/index"))
+					.andReturn();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(new Object() {
+			}.getClass().getEnclosingMethod().getName());
+		}
+
 	}
 
 }
